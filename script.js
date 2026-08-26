3f327fe7a47753732ec2ea78f583b8dc5481aea1
lValtiel <dev.emersonc@gmail.com>
2026-08-26 00:36:16 -0400

    Actualiza diseño y contenido del portafolio

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const btn = document.getElementById('button');
 const sectionAll = document.querySelectorAll('section[id]');
-const inputName = document.querySelector('#nombre');
-const inputEmail = document.querySelector('#email');
 const flagsElement = document.getElementById('flags');
 const textsToChange = document.querySelectorAll('[data-section]');
 
